8867a5de4ffa22a79d6fc93fe8d629df7224f7e8
Stephan <sgriesel@gmail.com>
2019-06-20 09:42:05 +0200

    change app to class based component

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="name">Name:</Label>
-        <Input 
-          type="text"
-          name="name"
+class App extends Component {
+  render() {
+    return (
+      <Form style={{ width: '50%' }}>
+        <FormGroup>
+          <Label for="name">Name:</Label>
+          <Input
+            type="text"
+            name="name"
           // onChange={this.handleChange} 
           />
-      </FormGroup>
+        </FormGroup>
 
-      <FormGroup>
-        <Label for="name">Email:</Label>
-        <Input 
-          type="email"
-          name="email"
+        <FormGroup>
+          <Label for="name">Email:</Label>
+          <Input
+            type="email"
+            name="email"
           // onChange={this.handleChange} 
           />
-      </FormGroup>
+        </FormGroup>
 
-      <FormGroup>
-        <Label for="name">Message:</Label>
-        <Input 
-          type="textarea"
-          name="message"
+        <FormGroup>
+          <Label for="name">Message:</Label>
+          <Input
+            type="textarea"
+            name="message"
           // onChange={this.handleChange} 
           />
-      </FormGroup>
+        </FormGroup>
 
-      <Button>Submit</Button>
-    </Form>
-  );
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
 }
 
 export default App;
